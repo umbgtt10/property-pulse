445a2ee03ff6b8c4208bd5d05e7ee79e6d9d404a
umbertogotti <umbertogotti1979@gmail.com>
2024-03-29 14:42:14 +0100

    Property Box and footer implemented

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Hero from "@/components/Hero"
+import InfoBoxes from "@/components/InfoBoxes"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+       <Hero />
+       <InfoBoxes />
+    </>
   )
 }
 
