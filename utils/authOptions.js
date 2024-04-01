c970e460d4ef7019c32c6b7b578b6dd8f048a5eb
umbertogotti <umbertogotti1979@gmail.com>
2024-04-01 17:38:45 +0200

    Bug in folder naming fixed

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,18 +1,18 @@
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
-    provider:[
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            authorization:{
-                params: {
-                    prompt: 'consent',
-                    access_type: 'offline',
-                    response_type: 'code',
-                }
-            }
-        })
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        authorization: {
+          params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
+          },
+        },
+      }),
     ],
     callbacks: {
         // Invoked on seccessful signin
@@ -21,12 +21,16 @@ export const authOptions = {
             // 2. Check if user exists
             // 3. If not, then add user to DB
             // 4. Return true to allow sign in
+            console.log(profile)
+            return true
         },
         // Modifies the session object
         async session({ session }) {
             // 1. Get user from DB
             // 2. Assign user id to session
             // 3. Return session
+            console.log(session)
+            return session
         }
     }
 }
