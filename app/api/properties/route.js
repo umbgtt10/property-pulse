5bd3854cd4aa1747d5958e23e4a7c22729b96ef7
umbertogotti <umbertogotti1979@gmail.com>
2024-04-05 13:07:42 +0200

    Image upload to cloudinary implemented

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from "@/config/database"
 import Property from "@/models/Property"
 import { getSessionUser } from "@/utils/getSessionUser"
+import cloudinary from '@/config/cloudinary'
 
 
 // GET /api/properties
@@ -59,7 +60,27 @@ export const POST = async (request) => {
                 phone: formData.get('seller_info.phone'),
             },
             owner: userId,
-            // images,
+        }
+
+        const imageUploadPromises = []
+
+        for(const image of images) {
+           const imageBuffer = await image.arrayBuffer() 
+           const imageArray = Array.from(new Uint8Array(imageBuffer))
+           const imageData = Buffer.from(imageArray)
+           const imageBase64 = imageData.toString('base64')
+           
+           console.log()
+
+           const result = await cloudinary.uploader.upload(
+            `data:image/pmg;base64,${imageBase64}`, {
+                folder:'property-pulse'
+            }
+           ) 
+
+           imageUploadPromises.push(result.secure_url)      
+           const uploadedImages = await Promise.all(imageUploadPromises)  
+           propertyData.images = uploadedImages
         }
 
         const newProperty = new Property(propertyData)
